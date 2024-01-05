6d2d9c3a1ab08853e0b04c418564d42410143ed8
JonnavithulaGirish <girish@transcend.io>
2024-01-05 08:12:51 -0500

    Use setConsent

--- app/src/main/java/io/transcend/samplesdk/HomeActivity.java
+++ app/src/main/java/io/transcend/samplesdk/HomeActivity.java
@@ -1,19 +1,20 @@
 package io.transcend.samplesdk;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+
+import android.os.Handler;
+import android.os.Looper;
+import androidx.preference.PreferenceManager;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import java.util.Set;
-
-import io.transcend.webview.TranscendConstants;
-import io.transcend.webview.models.TrackingConsentDetails;
 import io.transcend.webview.TranscendAPI;
-import io.transcend.webview.TranscendListener;
+import io.transcend.webview.TranscendConstants;
 import io.transcend.webview.TranscendWebView;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -21,70 +22,80 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        TranscendWebView wv = (TranscendWebView) findViewById(R.id.webView);
         WebView ewv= (WebView) findViewById(R.id.eshopit);
         ewv.getSettings().setJavaScriptEnabled(true);
         ewv.getSettings().setDomStorageEnabled(true);
-
-        ewv.loadUrl("https://eshopit.co/");
         ewv.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                // Execute JavaScript to update localStorage
-                try {
-                    TranscendAPI.getConsent(getApplicationContext(), new TranscendListener.ConsentListener() {
-                        @Override
-                        public void onConsentReceived(TrackingConsentDetails trackingConsentDetails) {
-                            System.out.println(trackingConsentDetails.getPurposes().get("Analytics"));
-                            try {
-                                TranscendAPI.setConsent(ewv, getApplicationContext(), trackingConsentDetails, new TranscendListener.ConsentStatusUpdateListener() {
-                                    @Override
-                                    public void onConsentStatusUpdate(boolean success, String errorDetails) {
-                                        if (success) {
-                                            ewv.evaluateJavascript(String.format("localStorage.getItem('%s')", TranscendConstants.STORAGE_CONSENT_KEY), result -> {
-                                                System.out.println(result);
-                                            });
-                                            ewv.evaluateJavascript(String.format("localStorage.getItem('%s')", TranscendConstants.STORAGE_TCF_KEY), result -> {
-                                                System.out.println(result);
-                                            });
-                                        }
-                                    }
-                                });
-                            }
-                            catch (Exception ex){
-                                System.out.println("lol"+ ex);
-
-                            }
-                        }
-
-                    });
-                } catch (Exception e) {
-                    System.out.println("Found error on getConsent()");
-                }
+                super.onPageFinished(view,url);
+                ewv.evaluateJavascript(String.format("localStorage.getItem('%s')", TranscendConstants.STORAGE_CONSENT_KEY), System.out::println);
+                ewv.evaluateJavascript(String.format("localStorage.getItem('%s')", TranscendConstants.STORAGE_TCF_KEY), System.out::println);
             }
         });
-        TranscendWebView  wv= (TranscendWebView) findViewById(R.id.webView);
+
+        wv.setOnCloseListener(() -> {
+            new Handler(Looper.getMainLooper()).post(() -> {
+                try {
+                    TranscendAPI.getConsent(getApplicationContext(), trackingConsentDetails -> {
+                        System.out.println("In onCloseListener::" + trackingConsentDetails.isConfirmed());
+                    });
+                }
+                catch (Exception ex){
+                    System.out.println("Exception");
+                }
+                ewv.setVisibility(View.VISIBLE);
+                ewv.loadUrl("https://eshopit.co/");
+            });
+        });
 
         try {
-            TranscendAPI.getRegimes(getApplicationContext(), new TranscendListener.RegimesListener() {
-                @Override
-                public void onRegimesReceived(Set<String> regimes) {
-                    System.out.println(regimes.toArray()[0]);
-                   if(regimes.contains("GDPR")){
-                    wv.setVisibility(View.VISIBLE);
-                    // Approach 2: Create webview dynamically with onCloseListener
-//                       TranscendWebView wv = new TranscendWebView(HomeActivity.this,"https://transcend-cdn.com/cm/a3b53de6-5a46-427a-8fa4-077e4c015f93/airgap.js", new TranscendListener.OnCloseListener() {
-//                           @Override
-//                           public void onClose() {
-//                                System.out.println("Closed");
-//                           }
-//                       });
-//                       setContentView(wv);
-
-                   }
+            TranscendAPI.getConsent(getApplicationContext(), trackingConsentDetails -> {
+                System.out.println("isConfirmed:: " + trackingConsentDetails.isConfirmed());
+                System.out.println(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(TranscendConstants.TRANSCEND_CONSENT_DATA, "lol"));
+                try {
+                    TranscendAPI.getRegimes(getApplicationContext(), regimes -> {
+                        System.out.println("regimes size:: " + regimes.size());
+                        if (regimes.contains("GDPR") && !trackingConsentDetails.isConfirmed()) {
+                            System.out.println("WebView Alive");
+                            wv.setVisibility(View.VISIBLE);
+                        }
+                        else {
+                            ewv.setVisibility(View.VISIBLE);
+                            ewv.loadUrl("https://eshopit.co/");
+                        }
+                    });
+                } catch (Exception e) {
+                    System.out.println("Found error on getRegimes()");
                 }
+                System.out.println(trackingConsentDetails.getPurposes().get("Analytics"));
+
             });
         } catch (Exception e) {
-            System.out.println("Found error on getRegimes()");
+            System.out.println("Found error on getConsent()");
         }
     }
+    // Execute JavaScript to update localStorage
+    //        try {
+    //            String tcString = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(IABConstants.IAB_TCF_TC_STRING, "");
+    //            TranscendAPI.setConsent(ewv, trackingConsentDetails,tcString, (TranscendListener.ConsentStatusUpdateListener) (success, errorDetails) -> {
+    //                if (success) {
+    //                    ewv.evaluateJavascript(String.format("localStorage.getItem('%s')", TranscendConstants.STORAGE_CONSENT_KEY), System.out::println);
+    //                    ewv.evaluateJavascript(String.format("localStorage.getItem('%s')", TranscendConstants.STORAGE_TCF_KEY), System.out::println);
+    //                }
+    //            });
+    //        }
+    //        catch (Exception ex){
+    //            System.out.println("lol"+ ex);
+    //
+    //        }
+    //          Approach 2: Create webView dynamically with onCloseListener
+    //                       TranscendWebView wv = new TranscendWebView(HomeActivity.this,"https://transcend-cdn.com/cm/a3b53de6-5a46-427a-8fa4-077e4c015f93/airgap.js", new TranscendListener.OnCloseListener() {
+    //                           @Override
+    //                           public void onClose() {
+    //                                System.out.println("Closed");
+    //                           }
+    //                       });
+    //                       setContentView(wv);
 }
--- app/src/main/java/io/transcend/samplesdk/MainActivity.java
+++ app/src/main/java/io/transcend/samplesdk/MainActivity.java
@@ -3,18 +3,24 @@ package io.transcend.samplesdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.transcend.webview.TranscendAPI;
+import io.transcend.webview.TranscendConstants;
 import io.transcend.webview.TranscendListener;
 import io.transcend.webview.TranscendWebView;
 import io.transcend.webview.models.ConsentStatus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
         ImageView iconImageView = (ImageView) findViewById(R.id.iconImageView);
         iconImageView.startAnimation(getZoomOutAnimation());
 //      Note: Belongs to Managed Consent Database demo Org
-        String url = "https://transcend-cdn.com/cm/63b35d96-a6db-436f-a1cf-ea93ae4be24e/airgap.js";
-        webView = TranscendAPI.init(getApplicationContext(), url, new TranscendListener.ViewListener(){
+        String url = "https://transcend-cdn.com/cm-test/63b35d96-a6db-436f-a1cf-ea93ae4be24e/airgap.js";
+        List<String> domainUrls = new ArrayList<>(Arrays.asList("https://eshopit.co/"));
+        webView = TranscendAPI.init(getApplicationContext(), url,domainUrls, new TranscendListener.ViewListener(){
             @Override
             public void onViewReady() {
                 try {
@@ -51,6 +58,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Create an Intent to start the SecondActivity
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.myButton1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to start the SecondActivity
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
             }
         });
