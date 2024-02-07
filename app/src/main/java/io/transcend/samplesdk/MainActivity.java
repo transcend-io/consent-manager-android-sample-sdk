bf475dcc47878be46d3546df9bba16913b54fd72
JonnavithulaGirish <girish@transcend.io>
2024-02-07 01:15:31 -0500

    Bookings demo

--- app/src/main/java/io/transcend/samplesdk/MainActivity.java
+++ app/src/main/java/io/transcend/samplesdk/MainActivity.java
@@ -18,6 +18,7 @@ import android.os.Looper;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -36,43 +37,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpBookingWebView();
         setUpTranscendWebView();
-        setUpButtons();
-    }
-
-    private void setUpButtons() {
-        Button button = (Button) findViewById(R.id.Home);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an Intent to start the SecondActivity
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button manageConsentButton = (Button) findViewById(R.id.manageConsentPreferences);
-        manageConsentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an Intent to start the SecondActivity
-                Intent intent = new Intent(MainActivity.this, ManageConsentPreferences.class);
-                startActivity(intent);
-            }
-        });
     }
 
     private void setUpTranscendWebView() {
         // Note: Belongs to Managed Consent Database demo Org
-        String url = "https://transcend-cdn.com/cm/63b35d96-a6db-436f-a1cf-ea93ae4be24e/airgap.js";
+        String url = "https://transcend-cdn.com/cm/90ab36ce-3da0-481c-ba8d-98eb820d716f/airgap.js";
         // Any additional domains you'd like to sync consent data to
-        List<String> domainUrls = new ArrayList<>(Arrays.asList("https://example.com/"));
         TranscendWebView transcendWebView = (TranscendWebView) findViewById(R.id.transcendWebView);
-        LinearLayout layout = (LinearLayout)findViewById(R.id.contentView);
 
         transcendWebView.setOnCloseListener(() -> {
             new Handler(Looper.getMainLooper()).post(() -> {
-                layout.setVisibility(View.VISIBLE);
                 try {
                     TranscendAPI.getConsent(getApplicationContext(), trackingConsentDetails -> {
                         System.out.println("In onCloseListener::" + trackingConsentDetails.isConfirmed());
@@ -85,42 +61,50 @@ public class MainActivity extends AppCompatActivity {
         });
 
         TranscendAPI.init(
-                getApplicationContext(),
-                url,
-                domainUrls,
-                new TranscendListener.ViewListener() {
-                    @Override
-                    public void onViewReady() {
-                        try {
-                            transcendInitialized = true;
-                            System.out.println("Transcend Ready!!!!!!!");
-                            TranscendAPI.getConsent(getApplicationContext(), trackingConsentDetails -> {
-                                System.out.println("isConfirmed: " + trackingConsentDetails.isConfirmed());
-                                System.out.println("SharedPreferences: " + PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(TranscendConstants.TRANSCEND_CONSENT_DATA, "lol"));
-                                System.out.println("GDPR_APPLIES from SharedPreferences: " + PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt(IABConstants.IAB_TCF_GDPR_APPLIES, 100));
+            getApplicationContext(),
+            url,
+            new TranscendListener.ViewListener() {
+                @Override
+                public void onViewReady() {
+                    try {
+                        System.out.println("Transcend Ready!!!!!!!");
+                        TranscendAPI.getConsent(getApplicationContext(), trackingConsentDetails -> {
+                            System.out.println("isConfirmed: " + trackingConsentDetails.isConfirmed());
+                            System.out.println("SharedPreferences: " + PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(TranscendConstants.TRANSCEND_CONSENT_DATA, "lol"));
+                            System.out.println("GDPR_APPLIES from SharedPreferences: " + PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt(IABConstants.IAB_TCF_GDPR_APPLIES, 100));
+                            getRegimeHandlerLogic(trackingConsentDetails, transcendWebView);
+                        });
 
-                                try {
-                                    TranscendAPI.getRegimes(getApplicationContext(), regimes -> {
-                                        System.out.println("regimes: " + regimes.toString());
-                                        if (regimes.contains("gdpr") && !trackingConsentDetails.isConfirmed()) {
-                                            System.out.println("Requesting user consent...");
-                                            transcendWebView.setVisibility(View.VISIBLE);
-                                        } else {
-                                            transcendWebView.hideConsentManager();
-                                            LinearLayout contentView = findViewById(R.id.contentView);
-                                            contentView.setVisibility(View.VISIBLE);
-                                        }
-                                    });
-                                } catch (Exception e) {
-                                    System.out.println("Found error on getRegimes()");
-                                }
-                            });
-
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
                 }
+            }
         );
+    }
+
+    private void setUpBookingWebView(){
+        WebView webView = (WebView) findViewById(R.id.WebView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("https://www.booking.com/");
+    }
+
+    private void getRegimeHandlerLogic(TrackingConsentDetails trackingConsentDetails,
+                                  TranscendWebView transcendWebView){
+        try {
+            TranscendAPI.getRegimes(getApplicationContext(), regimes -> {
+                System.out.println("regimes: " + regimes.toString());
+                // Custom logic
+                if (true) {
+                    System.out.println("Requesting user consent...");
+                    transcendWebView.setVisibility(View.VISIBLE);
+                } else {
+                    transcendWebView.hideConsentManager();
+                }
+            });
+        } catch (Exception e) {
+            System.out.println("Found error on getRegimes()");
+        }
     }
 }
