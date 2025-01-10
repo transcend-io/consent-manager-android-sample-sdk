cdf4a71ba6c653df9409346f65527392d73a0e93
JonnavithulaGirish <girish@transcend.io>
2025-01-10 15:14:48 -0500

    Sample kotlin usage

--- app/src/main/java/com/example/transcenddemoapplication/MainActivity.kt
+++ app/src/main/java/com/example/transcenddemoapplication/MainActivity.kt
@@ -2,40 +2,26 @@ package com.example.transcenddemoapplication
 
 import android.content.Context
 import android.os.Bundle
-import androidx.preference.PreferenceManager
-import android.util.Log
-import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.transcenddemoapplication.ui.theme.TranscendDemoApplicationTheme
-import io.transcend.webview.TranscendConstants
-import io.transcend.webview.TranscendListener
 import io.transcend.webview.TranscendWebView
 
 class MainActivity : ComponentActivity() {
@@ -67,7 +53,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TranscendDemoApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = Color.Blue) {
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
                     if(isApiInstanceReady.value) {
                         ShowButtons(applicationContext, isConsentConfirmed.value)
                     }
@@ -108,7 +94,6 @@ fun ShowButtons(applicationContext: Context, isConsentConfirmed: Boolean) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier
-            .background(Color.White)
             .padding(16.dp)
             .height(48.dp)
     ) {
